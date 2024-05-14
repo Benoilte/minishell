3da4392faefe9504f43b586609767e032151a158
Bebrandt <benoit.07@hotmail.com>
2024-05-14 20:45:35 +0200

    modify minishell.c main function

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,27 +6,22 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:29:53 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/14 18:04:18 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/14 20:24:26 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
-	ft_printf("main function for minishell\n");
-	ft_printf("\n");
-	prompt();
-	ft_printf("\n");
-	lexer();
-	ft_printf("\n");
-	parser();
-	ft_printf("\n");
-	builtins();
-	ft_printf("\n");
-	exec();
-	ft_printf("\n");
-	struct_utils();
-	ft_printf("\n");
-	test();
+	t_bash	*bash;
+
+	(void)argv;
+	if (argc != 1)
+		ft_printf("run the program without argument as follow: ./minishell\n");
+	else
+	{
+		init_bash(&bash, envp);
+	}
+	return (EXIT_SUCCESS);
 }
