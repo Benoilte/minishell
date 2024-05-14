88bf54be5d724d3b0b92b15f45524afc80401010
Bebrandt <benoit.07@hotmail.com>
2024-05-14 17:43:01 +0200

    test struct creation

--- srcs/main/main_ben.c
+++ srcs/main/main_ben.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:18:33 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/14 15:47:09 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/14 17:30:51 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int	main(void)
 {
+	t_instructions	inst;
+
+	inst.fd[0] = 1;
+	inst.fd[1] = 2;
 	ft_printf("main function for ben\n");
 	prompt();
+	ft_printf("\n");
+	ft_printf("fd[0] = %d\n", inst.fd[0]);
+	ft_printf("fd[0] = %d\n", inst.fd[1]);
 }
