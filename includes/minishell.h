5b7db0ae244dd1526c2df08979dabc0d9a3ef92b
Bebrandt <benoit.07@hotmail.com>
2024-05-14 17:41:53 +0200

    add struct.h into minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,16 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:16:35 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/14 13:02:30 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/14 17:23:40 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+// PERSONNAL LIB
+
+# include "struct.h"
 # include "../libft/includes/libft.h"
 
 // main/minishell.c
