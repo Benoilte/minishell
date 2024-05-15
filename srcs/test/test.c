0a87e21c5542bc64f494a291f0a60a5ba35eff75
benoit brandt <benoit.07@hotmail.com>
2024-05-15 14:12:34 +0200

    create test function to print struct env

--- srcs/test/test.c
+++ srcs/test/test.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 12:54:57 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/14 12:57:09 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:49:58 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	test(void)
+void	test_print_env(t_env *env)
 {
-	ft_printf("test function\n");
+	t_env	*tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		ft_printf("%s%s\n", tmp->name, tmp->value);
+		tmp = tmp->next;
+	}
 }
