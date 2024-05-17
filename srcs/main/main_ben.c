613639b6a5466eaa8707a37d8d0740ac5381a83b
Bebrandt <benoit.07@hotmail.com>
2024-05-17 16:52:05 +0200

    adapt main for test

--- srcs/main/main_ben.c
+++ srcs/main/main_ben.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_ben.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:18:33 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/16 16:55:27 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:48:06 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_bash	*bash;
+	char	*sequence;
+	int		input_is_not_exit;
 
 	(void)argv;
 	if (argc != 1)
@@ -22,7 +24,24 @@ int	main(int argc, char *argv[], char *envp[])
 	else
 	{
 		init_bash(&bash, envp);
-		test_print_env(bash->env);
+			input_is_not_exit = 1;
+		while (input_is_not_exit)
+		{
+			sequence = readline("minishell> ");
+			if (ft_strlen(sequence) == 0)
+				printf("%s", sequence);
+			else if (ft_strncmp(sequence, "exit", ft_strlen(sequence)) == 0)
+				break ;
+			else if (ft_strncmp(sequence, "env", ft_strlen(sequence)) == 0)
+				test_print_env(bash->env);
+			else
+			{
+				lexing(bash, sequence);
+				clear_instruction(&(bash)->instruction);
+			}
+			free(sequence);		
+		}
+		free (sequence);
 		clear_bash(&bash);
 	}
 	return (EXIT_SUCCESS);
