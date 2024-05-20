dde5d5c24a1cab43d3a0d0e4951574b8cda75331
Bebrandt <benoit.07@hotmail.com>
2024-05-20 14:20:18 +0200

    add function to check the syntax of the cmd data

--- includes/parser.h
+++ includes/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 13:48:16 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/19 23:59:17 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:14:25 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,19 @@ void	update_instruction(t_bash *bash, t_instruction *instruction);
 int		check_redirections(t_token *red);
 int		check_data_redirection(char *data, int type);
 int		check_opt_redirection(char *option, int type);
-int		is_unexpected_token(char c);
+int		unexpected_red_token(char c);
 
 // parser/parser_check_cmd.c
+
+int		check_cmd(t_token *cmd);
+int		check_data_cmd(char *data, int type);
+int		unexpected_cmd_token(char c);
 
 // parser/parser_check_utils.c
 
 int		type_equal_to(int flag, int type);
 int		check_closed_quotes(char *data);
-int		check_text_out_of_quotes(char *data);
+int		check_text_out_of_quotes(char *data, int (*is_unexpected_token)(char));
 int		print_parsing_error_msg(char *msg, char *token_str, char token_c);
 
 #endif
