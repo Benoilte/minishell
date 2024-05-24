091a91df3308d4aa746af493ed254c5dfe91a142
benoit brandt <benoit.07@hotmail.com>
2024-05-24 18:12:21 +0200

    debug get_value() and comparing only the bigger word

--- srcs/struct_utils/env_utils.c
+++ srcs/struct_utils/env_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:53:44 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/24 17:25:34 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/05/24 18:05:45 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 char	*get_value(t_env *env, char *name)
 {
 	t_env	*tmp;
+	int		len;
 
 	tmp = env;
 	while (tmp)
 	{
-		if (ft_strncmp(tmp->name, name, ft_strlen(name)) == 0)
+		if (ft_strlen(name) > ft_strlen(tmp->name))
+			len = ft_strlen(name);
+		else
+			len = ft_strlen(tmp->name);
+		if (ft_strncmp(tmp->name, name, len) == 0)
 			return (ft_strdup(tmp->value));
 		tmp = tmp->next;
 	}
