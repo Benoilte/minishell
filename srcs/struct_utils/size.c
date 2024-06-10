88d905479cff00759b71cfdf4937f22be3c7f0ce
Bebrandt <benoit.07@hotmail.com>
2024-06-10 17:19:49 +0200

    give back the promt if a command is empty, or is it filled only with space char

--- srcs/struct_utils/size.c
+++ srcs/struct_utils/size.c
@@ -6,11 +6,26 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:29:37 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/05/21 16:31:39 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:08:48 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int	size_instruction(t_instruction *instruction)
+{
+	int				i;
+	t_instruction	*tmp;
+
+	i = 0;
+	tmp = instruction;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
 
 int	size_token(t_token *token)
 {
