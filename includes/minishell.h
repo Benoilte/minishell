4b03f9abf6e1b2fa69b11e216150764a6b4a081c
benoit brandt <benoit.07@hotmail.com>
2024-06-13 12:50:02 +0200

    solve conflict

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:16:35 by bebrandt          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/06/12 14:36:42 by tmartin2         ###   ########.fr       */
-=======
-/*   Updated: 2024/06/10 16:20:11 by bebrandt         ###   ########.fr       */
->>>>>>> 70c573884fac4784705ef80df1aecba5784049bb
+/*   Updated: 2024/06/13 12:47:24 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,11 +63,7 @@ int		parsing(t_bash *bash);
 
 // builtins/builtins.c
 
-<<<<<<< HEAD
 void builtins(t_instruction *instruction, t_env *env, t_bash *bash);
-=======
-void	builtins(t_instruction *instruction, t_env *env);
->>>>>>> 70c573884fac4784705ef80df1aecba5784049bb
 
 // exec/exec.c
 
